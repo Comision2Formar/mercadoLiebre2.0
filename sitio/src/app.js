0311f54e2994b6203deb244e52ef2bc5642f0f7e
Eric Mena <menaericdaniel@gmail.com>
2020-09-22 13:30:11 -0300

    crear y leer usuarios (CRUD)

--- sitio/src/app.js
+++ sitio/src/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 const methodOverride = require('method-override');
 const session = require('express-session');
+const localsUserCheck = require('./middlewares/localsUserCheck');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(methodOverride('_method'));
 app.use(session({secret:'mercado liebre for ever'}));
+app.use(localsUserCheck);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- sitio/src/controllers/usersControllers.js
+++ sitio/src/controllers/usersControllers.js
@@ -1,6 +1,8 @@
 const dbProducts = require('../data/database');
 const dbUsers = require('../data/dbUsers');
 
+const db = require('../database/models');
+
 const {validationResult, body} = require('express-validator');
 const bcrypt =require('bcrypt');
 const fs = require('fs');
@@ -18,30 +20,26 @@ module.exports = {
     },
     processRegister:function(req,res){
         let errors = validationResult(req);
-        let lastID = 0;
-        if(dbUsers.length > 0){
-            dbUsers.forEach(user=>{
-                if(user.id > lastID){
-                    lastID = user.id
-                }
-            })
-        }
-
+     
         if(errors.isEmpty()){
-            let nuevoUsuario = {
-                id:lastID+1,
-                nombre:req.body.nombre,
-                apellido:req.body.apellido,
-                email:req.body.email,
-                avatar:(req.files[0])?req.files[0].filename:"default.png",
-                pass:bcrypt.hashSync(req.body.pass,10),
-                rol:"user"
-            }
-
-            dbUsers.push(nuevoUsuario);
-
-            fs.writeFileSync(path.join(__dirname,'..','data','dbUsers.json'),JSON.stringify(dbUsers),'utf-8')
-            return res.redirect('/users/login')
+           
+            db.Users.create(
+                {
+                    nombre:req.body.nombre.trim(),
+                    apellido:req.body.apellido.trim(),
+                    email:req.body.email.trim(),
+                    password:bcrypt.hashSync(req.body.pass.trim(),10),
+                    avatar:(req.files[0])?req.files[0].filename:"default.png",
+                    rol:"user"
+                }
+            )
+            .then(result => {
+                console.log(result)
+                return res.redirect('/users/login');
+            })
+            .catch(errores => {
+                console.log(errores)
+            })
         }else{
             res.render('userRegister',{
                 title:"Registro de Usuarios",
@@ -57,28 +55,35 @@ module.exports = {
             title:"Ingreso de Usuarios",
             css:'index.css',
             user:req.session.user
-
-
         })
     },
     processLogin:function(req,res){
         let errors = validationResult(req);
         if(errors.isEmpty()){
-            dbUsers.forEach(usuario=>{
-                if(usuario.email == req.body.email){
-                    req.session.user = {
-                        id:usuario.id,
-                        nick:usuario.nombre + ' ' + usuario.apellido,
-                        rol:usuario.rol,
-                        email:usuario.email,
-                        avatar:usuario.avatar
-                    }
+     
+            db.Users.findOne({
+                where:{
+                    email:req.body.email
                 }
             })
-            if(req.body.recordar){
-                res.cookie('userMercadoLiebre',req.session.user,{maxAge:1000*60*2})
-            }
-            return res.redirect('/')
+            .then(user => {
+                req.session.user = {
+                    id: user.id,
+                    nick: user.nombre +  " " + user.apellido,
+                    email: user.email,
+                    avatar: user.avatar,
+                    rol: user.rol
+                }
+
+                if(req.body.recordar){
+                    res.cookie('userMercadoLiebre',req.session.user,{maxAge:1000*60*2})
+                }
+
+                res.locals.user = req.session.user;
+
+                return res.redirect('/')
+            })
+
         }else{
             return res.render('userLogin',{
                 title:"Ingreso de Usuarios",
@@ -98,15 +103,22 @@ module.exports = {
         res.redirect('/')
     },
     profile:function(req,res){
-        res.render('userProfile',{
-            title:"Perfil de Usuario",
-            css:'index.css',
-            user:req.session.user,
-            user:req.session.user,
-            productos: dbProducts.filter(producto=>{
-                return producto.category != "visited" && producto.category != "in-sale"
+        if(req.session.user){
+            db.Users.findByPk(req.session.user.id)
+            .then(user => {
+                res.render('userProfile',{
+                    title:"Perfil de Usuario",
+                    css:'index.css',
+                    usuario:user,                    
+                    productos: dbProducts.filter(producto=>{
+                        return producto.category != "visited" && producto.category != "in-sale"
+                    })
+        
+                })
             })
-
-        })
+        }else{
+            return res.redirect('/')
+        }
+       
     }
 }
